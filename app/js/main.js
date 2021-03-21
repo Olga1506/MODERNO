9d67d79d6a7b9adb8b2aeb0350a7e8cbdd17e6e9
Olga1506 <bolga1626@gmail.com>
2021-03-21 19:31:06 +0200

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -7,9 +7,21 @@ $('.product-slider__inner').slick({
     slidesToScroll:4,
 });
 
+
+$('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
+});
+
+    $('.header__btn-menu').on('click', function () {
+        $('.header__box').toggleClass('active');
+    });
+
+
+
+
     var mixer = mixitup('.products__inner-box');
 
 
 
 
-})
+});
